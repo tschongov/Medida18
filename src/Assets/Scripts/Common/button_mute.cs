37bce38874506c1d2682e651cf7c72eace24ff9d
tschongov <a.tschongov@gmail.com>
2018-06-28 20:40:49 +0200

    Resources folder structured properly now

---
+++ src/Assets/Scripts/Common/button_mute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class button_mute : MonoBehaviour {
+
+	Sprite mute;
+	Sprite muted;
+
+	public void Start(){
+		mute = Resources.Load<Sprite>("Graphics/mute");
+		muted = Resources.Load<Sprite>("Graphics/muted");
+	}
+
+	public void mute_game(Button button) {
+
+		AudioListener.pause = !AudioListener.pause;
+
+		if (AudioListener.pause)
+			button.GetComponent<Image>().sprite = muted;
+		else
+			button.GetComponent<Image>().sprite = mute;
+	}
+
+}
---
+++ src/Assets/Scripts/Introvideo/video_start.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class video_start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//WaitForSeconds(3);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

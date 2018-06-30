04ce523e68634a4c60fb239e1765cd622e6bfed4
Tung <dothanhtung@gmx.de>
2018-06-30 05:57:03 +0200

    update

---
+++ src/Assets/Resources/Scripts/Mojito/Stamp_Lime/Drag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    void Update()
+    {
+
+      
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = (pos);
+    }
+}
+
+
+
+
+

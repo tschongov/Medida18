da4f13b634577eb5e0bd412ba07e25c0663e551f
Thanh Tung Do <dothanhtung@gmx.de>
2018-07-15 08:40:36 +0200

    change

---
+++ src/Assets/Resources/Scripts/Mojito/Muddle/Muddlen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muddlen : MonoBehaviour
+{
+    public float speed = 20f;
+    
+    Rigidbody2D rig;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Fire1");
+        float vertical = Input.GetAxis("Mouse Y");
+        Vector2 move = new Vector2(horizontal, vertical) * speed * Time.deltaTime;
+        rig.MovePosition(rig.position + move);
+
+    }
+}
+
---
+++ src/Assets/Resources/Scripts/Mojito/Muddle/Trigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+    public GameObject Hook;
+
+    void Start()
+    {
+        Hook.SetActive(false);
+    }
+
+
+    public int count = 0;
+    void OnCollisionEnter2D()
+    {
+        if (count >= 10)
+            {
+            Hook.SetActive(true);
+            }
+        else
+        {
+            count++;
+        }
+    }
+}
